c6cfe18220c0a8d9943fa72adc08568de59d4d19
Vinicius Cardoso <vinicius@Viniciuss-MacBook-Air.local>
2019-03-23 19:55:49 -0300

    v0.1

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const helmet = require('helmet')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+console.log(mongoose.version)
+
+const app = express()
+
+mongoose.Promise = global.Promise
+mongoose.connect(process.env.dbURL || 'mongodb://core-db:27017/core', {useNewUrlParser: true})
+
+const port = process.env.PORT || 3000
+const server = require('http').Server(app)
+
+app.get('/', (req, res) => {
+    return res.status(200).json({msg: 'Welcome to the API'})
+})
+
+server.listen(port)
